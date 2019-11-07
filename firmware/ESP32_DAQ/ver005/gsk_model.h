605556c9d7bf1d14c5475de8b14b4db0f2d4dc64
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2019-11-07 00:42:19 -0600

    Automatic push

---
+++ firmware/ESP32_DAQ/ver005/gsk_model.h
@@ -0,0 +1,185 @@
+#include <ArduinoJson.h>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
+
+struct PORT {
+  String Name;
+  int Pin;
+};
+
+struct VAR {
+  String Name;
+  double Value;
+};
+
+struct MODEL {
+  vector <VAR> Params;
+  vector <VAR> States;
+  vector <PORT> Inputs;
+  vector <PORT> Outputs;
+  double T_S=0.001;
+} System;
+
+PORT newPORT(String Name, int Pin) {
+  PORT Out;
+  Out.Name = Name;
+  Out.Pin = Pin;
+  return Out;
+}
+
+VAR newVAR(String Name, double Value) {
+  VAR Out;
+  Out.Name = Name;
+  Out.Value = Value;
+  return Out;
+}
+
+StaticJsonDocument<300> JsonCodeIn;
+StaticJsonDocument<300> JsonCodeOut;
+
+void readParamsFromEEPROM() {
+  int count = System.Params.size();
+  int Address = 0;
+  for (char i=0; i<count; i++) {
+    System.Params[i].Value = EEPROM.readDouble(Address);
+    Address += sizeof(double);
+  }
+}
+
+void AppendAState(String Name, double Value) {
+  System.States.push_back(newVAR(Name, Value));
+}
+
+void AppendAParam(String Name, double Value) {
+  System.Params.push_back(newVAR(Name, Value));
+}
+
+void AppendAnInputPort(String Name, int Pin) {
+  System.Inputs.push_back(newPORT(Name, Pin));
+}
+
+void AppendAnOutputPort(String Name, int Pin) {
+  System.Outputs.push_back(newPORT(Name, Pin));
+}
+
+void rt_loop (MODEL &Sys);
+void handleInputMessage();
+
+void realtimeActivity(void *GSystem) {
+  bool LEDState = true;
+  // Initialise the xLastWakeTime variable with the current time.
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  //TickType_t xFrequency = pdMS_TO_TICKS(((MODEL*) GSystem)->States[0].Value);
+  //MODEL Sys = (MODEL *)GSystem;
+  for (;;) {
+    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(((MODEL*) GSystem)->T_S*1000));
+    
+    rt_loop(*(MODEL *) GSystem);
+  }
+}
+
+
+void gskInit() {
+  Serial.begin(1500000);
+  readParamsFromEEPROM();
+  xTaskCreatePinnedToCore(realtimeActivity, "realtimeActivity", 32768, (void*)&System, 2,  NULL,  ARDUINO_RUNNING_CORE);
+}
+
+char InChars[100];
+//String InItem;
+void loop() {
+  int availChars = Serial.available();
+  if (availChars) {
+    availChars = (availChars<100)?availChars:100;
+    //Serial.readBytes(InChars, availChars);
+    //const String InItem = Serial.readString();
+    // Deserialize the JSON document
+    DeserializationError error = deserializeJson(JsonCodeIn, Serial);
+    if (!error) {
+      handleInputMessage();
+      serializeJson(JsonCodeOut, Serial);
+      JsonCodeIn.clear();
+    } else {
+      Serial.println("opss");
+    }
+  }
+}
+
+void handleInputMessage() {
+  int cmd = JsonCodeIn["A"];
+  JsonCodeOut.clear();
+  JsonCodeOut["B"] = cmd;
+  switch (cmd) {
+    case 0:  { //Count number of states
+      JsonCodeOut["v"] = System.States.size();
+    }
+    break;
+    case 1: { //Read state
+      int i = JsonCodeIn["i"];
+      if (i>=0 && i<System.States.size()) {
+        JsonCodeOut["i"] = i;
+        JsonCodeOut["v"] = System.States[i].Name;
+      }
+    }
+    break;
+    case 2: { //Read sValue
+      int i = JsonCodeIn["i"];
+      if (i>=0 && i<System.States.size()) {
+        JsonCodeOut["i"] = i;
+        JsonCodeOut["v"] = System.States[i].Value;
+      }
+    }
+    break;
+    case 3: { //Read pCount
+      int i = JsonCodeIn["i"];
+      if (i>=0 && i<System.States.size()) {
+        JsonCodeOut["v"] = System.Params.size();
+      }
+    }
+    break;
+    case 4: { //Read param
+      int i = JsonCodeIn["i"];
+      if (i>=0 && i<System.Params.size()) {
+        JsonCodeOut["i"] = i;
+        JsonCodeOut["v"] = System.Params[i].Name;
+      }
+    }
+    break;
+    case 5: { //Read pValue
+      int i = JsonCodeIn["i"];
+      if (i>=0 && i<System.Params.size()) {
+        JsonCodeOut["i"] = i;
+        JsonCodeOut["v"] = System.Params[i].Value;
+      }
+    }
+    break;
+    case 6: { //write pValue
+      int i = JsonCodeIn["i"];
+      double val = JsonCodeIn["v"];
+      if (i>=0 && i<System.Params.size()) {
+        System.Params[i].Value = val;
+        JsonCodeOut["i"] = i;
+        JsonCodeOut["v"] = val;
+      }
+    }
+    break;
+    case 7: { //Read ts
+      JsonCodeOut["v"] = System.T_S;
+    }
+    break;
+    case 8: { //write ts
+      double val = JsonCodeIn["v"];
+      System.T_S = val;
+      JsonCodeOut["v"] = System.T_S;
+    }
+    break;
+  }
+  
+}
