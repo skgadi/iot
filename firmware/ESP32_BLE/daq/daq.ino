3c457b6f3410de27e038c75486632f76e1e226bd
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2019-12-06 08:38:27 -0600

    Automatic push

---
+++ firmware/ESP32_BLE/daq/daq.ino
@@ -0,0 +1,77 @@
+/*Program to use GATT service on ESP32 to send Battery Level
+ * ESP32 works as server - Mobile as client
+ * Program by: B.Aswinth Raj
+ * Dated on: 13-10-2018
+ * Website: www.circuitdigest.com
+ */
+
+ 
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h> //Library to use BLE as server
+#include <BLE2902.h> 
+
+ 
+
+bool _BLEClientConnected = false;
+
+#define BatteryService BLEUUID((uint16_t)0x180F) 
+BLECharacteristic BatteryLevelCharacteristic(BLEUUID((uint16_t)0x2A19), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic ValueCharac(BLEUUID((uint16_t)0x2A18), BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
+//BLEDescriptor BatteryLevelDescriptor(BLEUUID((uint16_t)0x2901));
+
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      _BLEClientConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      _BLEClientConnected = false;
+    }
+};
+
+void InitBLE() {
+  BLEDevice::init("CIT-Bridge");
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pBattery = pServer->createService(BatteryService);
+
+  pBattery->addCharacteristic(&BatteryLevelCharacteristic);
+  pBattery->addCharacteristic(&ValueCharac);
+  //BatteryLevelDescriptor.setValue("Percentage 0 - 100");
+  //BatteryLevelCharacteristic.addDescriptor(&BatteryLevelDescriptor);
+  BatteryLevelCharacteristic.addDescriptor(new BLE2902());
+
+  pServer->getAdvertising()->addServiceUUID(BatteryService);
+
+  pBattery->start();
+  // Start advertising
+  pServer->getAdvertising()->start();
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Battery Level Indicator - BLE");
+  InitBLE();
+}
+
+  uint8_t level = 57;
+  char af[100];
+void loop() {
+
+  BatteryLevelCharacteristic.setValue(&level, 1);
+  BatteryLevelCharacteristic.notify();
+  //String a = ValueCharac.getValue().c_str();
+  Serial.println(ValueCharac.getValue().c_str());
+  delay(5000);
+
+  level++;
+  Serial.println(int(level));
+
+  if (int(level)==100)
+  level=0;
+
+}
